eb001e0058d1c9a7ac7c88cb4f9dc03051a910d7
Karl Lapo <karl.lapo@uni-bayreuth.de>
2018-10-17 10:52:29 +0200

    Changed my email address.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='statsray',
    author="Karl Lapo; Nic Wayand",
-   author_email="lapok@atmos.washington.edu; nic.wayand@usask.ca>",
+   author_email="karl.lapo@uni-bayreuth.de; nic.wayand@usask.ca>",
    description="Statistics functions for the xarray package",
    version='0.1',
    packages=["statsray"])
